3a399943543c6bbbeb2ab8ce98a0a2557574daf5
Fer Boschi <fboschi.dg@gmail.com>
2020-10-22 09:13:03 -0300

    'cambio'

--- src/main/java/es/upm/miw/iwvg_devops/jgit/jgit.java
+++ src/main/java/es/upm/miw/iwvg_devops/jgit/jgit.java
@@ -1,20 +1,32 @@
 package es.upm.miw.iwvg_devops.jgit;
 
 
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.RemoteAddCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class jgit {
-    FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-    Repository repository = repositoryBuilder.setGitDir(new File("/path/to/repo/.git"))
-            .readEnvironment() // scan environment GIT_* variables
-            .findGitDir() // scan up the file system tree
-            .setMustExist(true)
-            .build();
 
-    public jgit() throws IOException {
+    private String localPath, remotePath;
+    private Git git;
+    public static void main(String[] args) throws IOException, GitAPIException, URISyntaxException {
+    }
+        public void checkout() throws IOException, NoFilepatternException, GitAPIException {
+        git.checkout()
+                .setCreateBranch(true)
+                .setName("Entidades")
+                .call();
     }
 }
