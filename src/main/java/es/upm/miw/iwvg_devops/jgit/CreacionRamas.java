4d1133f5cf1ff594b90f7f468fc307eb248e102c
Fer Boschi <fboschi.dg@gmail.com>
2020-10-23 14:58:36 -0300

    'nuevo'

---
+++ src/main/java/es/upm/miw/iwvg_devops/jgit/CreacionRamas.java
@@ -0,0 +1,189 @@
+package es.upm.miw.iwvg_devops.jgit;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.RemoteAddCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class CreacionRamas {
+    public static void Ramas() throws IOException, GitAPIException, URISyntaxException {
+        for (int x = 0; x <= 5; x++) {
+
+            switch(x){
+
+                case 1:try {
+                    String repoPath = "https://github.com/FerBoschi/Jgit-spab";
+                    String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
+                    Repository localRepo = new FileRepository(localPath);
+
+
+                    try (Git git = Git.init().setDirectory(new File(localPath)).call()) {
+
+
+                        git.checkout()
+                                .setCreateBranch(true)
+                                .setName("Farruco")
+                                .call();
+
+                        // agregar al repositorio remoto
+                        RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                        remoteAddCommand.setName("origin");
+                        remoteAddCommand.setUri(new URIish(repoPath));
+
+                        // Podemos agregar mas configuraciones si es necesario aca
+                        remoteAddCommand.call();
+
+                        // hacer push al remoto:
+                        PushCommand push = git.push();
+                        push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+                        // podemos agregar mas configuraciones aca si es necesario
+                        push.call();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                    break;
+
+                case 2:try {
+                    String repoPath = "https://github.com/FerBoschi/Jgit-spab";
+                    String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
+                    Repository localRepo = new FileRepository(localPath);
+
+
+                    try (Git git = Git.init().setDirectory(new File(localPath)).call()) {
+
+
+                        git.checkout()
+                                .setCreateBranch(true)
+                                .setName("Atr")
+                                .call();
+
+                        // agregar al repositorio remoto
+                        RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                        remoteAddCommand.setName("origin");
+                        remoteAddCommand.setUri(new URIish(repoPath));
+
+                        // Podemos agregar mas configuraciones si es necesario aca
+                        remoteAddCommand.call();
+
+                        // hacer push al remoto:
+                        PushCommand push = git.push();
+                        push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+                        // podemos agregar mas configuraciones aca si es necesario
+                        push.call();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                    break;
+
+                case 3:try {
+                    String repoPath = "https://github.com/FerBoschi/Jgit-spab";
+                    String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
+                    Repository localRepo = new FileRepository(localPath);
+
+
+                    try (Git git = Git.init().setDirectory(new File(localPath)).call()) {
+
+
+                        git.checkout()
+                                .setCreateBranch(true)
+                                .setName("DonaldTrump")
+                                .call();
+
+                        // agregar al repositorio remoto
+                        RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                        remoteAddCommand.setName("origin");
+                        remoteAddCommand.setUri(new URIish(repoPath));
+
+                        // Podemos agregar mas configuraciones si es necesario aca
+                        remoteAddCommand.call();
+
+                        // hacer push al remoto:
+                        PushCommand push = git.push();
+                        push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+                        // podemos agregar mas configuraciones aca si es necesario
+                        push.call();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                    break;
+
+                case 4:try {
+                    String repoPath = "https://github.com/FerBoschi/Jgit-spab";
+                    String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
+                    Repository localRepo = new FileRepository(localPath);
+
+
+                    try (Git git = Git.init().setDirectory(new File(localPath)).call()) {
+
+
+                        git.checkout()
+                                .setCreateBranch(true)
+                                .setName("MiVillanoFavorito")
+                                .call();
+
+                        // agregar al repositorio remoto
+                        RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                        remoteAddCommand.setName("origin");
+                        remoteAddCommand.setUri(new URIish(repoPath));
+
+                        // Podemos agregar mas configuraciones si es necesario aca
+                        remoteAddCommand.call();
+
+                        // hacer push al remoto:
+                        PushCommand push = git.push();
+                        push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+                        // podemos agregar mas configuraciones aca si es necesario
+                        push.call();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                    break;
+                case 5:try {
+                    String repoPath = "https://github.com/FerBoschi/Jgit-spab";
+                    String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
+                    Repository localRepo = new FileRepository(localPath);
+
+
+                    try (Git git = Git.init().setDirectory(new File(localPath)).call()) {
+
+
+                        git.checkout()
+                                .setCreateBranch(true)
+                                .setName("SeñorYSeñoraSmith")
+                                .call();
+
+                        // agregar al repositorio remoto
+                        RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                        remoteAddCommand.setName("origin");
+                        remoteAddCommand.setUri(new URIish(repoPath));
+
+                        // Podemos agregar mas configuraciones si es necesario aca
+                        remoteAddCommand.call();
+
+                        // hacer push al remoto:
+                        PushCommand push = git.push();
+                        push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+                        // podemos agregar mas configuraciones aca si es necesario
+                        push.call();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                    break;
+            }
+        }
+    }
+
+}
--- src/main/java/es/upm/miw/iwvg_devops/jgit/jgit.java
+++ src/main/java/es/upm/miw/iwvg_devops/jgit/jgit.java
@@ -1,62 +1,60 @@
 package es.upm.miw.iwvg_devops.jgit;
 
-import org.eclipse.jgit.api.*;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.api.errors.TransportException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.URIish;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.api.AddCommand;
+        import org.eclipse.jgit.api.Git;
+        import org.eclipse.jgit.api.PushCommand;
+        import org.eclipse.jgit.api.RemoteAddCommand;
+        import org.eclipse.jgit.api.errors.GitAPIException;
+        import org.eclipse.jgit.api.errors.TransportException;
+        import org.eclipse.jgit.internal.storage.file.FileRepository;
+        import org.eclipse.jgit.lib.Repository;
+        import org.eclipse.jgit.transport.URIish;
+        import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
+        import java.io.File;
+        import java.io.IOException;
+        import java.net.URISyntaxException;
 
+        import static es.upm.miw.iwvg_devops.jgit.CreacionRamas.Ramas;
 public class jgit {
-
     public static void main(String[] args) throws IOException, GitAPIException, URISyntaxException {
 
         try{
-            String httpUrl = "https://github.com/FerBoschi/Jgit-spab";
+            String repoPath = "https://github.com/FerBoschi/Jgit-spab";
             String localPath = "C:\\Users\\Cesar\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELEASE\\spab";
             Repository localRepo = new FileRepository(localPath);
-            try (Git git = Git.open(new File(localPath))) {
+            int i=0;
+
+            try (Git git = Git.init().setDirectory(new File(localPath)).call()){
 
                 AddCommand add = git.add();
                 add.addFilepattern(".").call();
-                git.commit().setMessage("Added myfile").call();
-
+                git.commit().setMessage("Added Persona").call();
 
                 // agregar al repositorio remoto
                 RemoteAddCommand remoteAddCommand = git.remoteAdd();
                 remoteAddCommand.setName("origin");
-                remoteAddCommand.setUri(new URIish(httpUrl));
+                remoteAddCommand.setUri(new URIish(repoPath));
 
                 // Podemos agregar mas configuraciones si es necesario aca
                 remoteAddCommand.call();
 
                 // hacer push al remoto:
-                PushCommand pushCommand = git.push();
-                pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("poner usuario de github", "poner contraseña"));
+                PushCommand push = git.push();
+                push.setCredentialsProvider(new UsernamePasswordCredentialsProvider("USER", "CONTRA"));
+
                 // podemos agregar mas configuraciones aca si es necesario
-                pushCommand.call();
+                push.call();
+
+                //RealizaLaCreacionDeLasRamas
+                Ramas();
             }
+
 
 
         }catch (Exception ex){
             System.out.println(ex.getMessage());
         }
 
-    }
-
-    private String localPath, remotePath;
-    private Git git;
-    
-    public void checkout() throws IOException, GitAPIException {
-        git.checkout()
-                .setCreateBranch(true)
-                .setName("Entidades")
-                .call();
     }
 }
